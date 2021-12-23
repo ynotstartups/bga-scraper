d9ca4327b61d879ef33b3d6e6f4ab6e6b58cd874
tiger.huang <tiger.huang@ly.st>
2021-12-23 09:01:12 +0000

    add parser and result

---
+++ parser.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import requests
+from bs4 import BeautifulSoup
+
+# TODO: python library cookiecutter?
+
+BASE_URL = "https://boardgamearena.com"
+
+all_games_url = f"{BASE_URL}/gamelist?section=all"
+all_games_page = requests.get(all_games_url)
+
+all_games_soup = BeautifulSoup(all_games_page.text, 'html.parser')
+
+for game in all_games_soup.find_all("a", class_="gamelist_link_to_game"):
+    game_href = game.get("href")
+
+    game_url = f"{BASE_URL}{game_href}"
+
+    game_page = requests.get(game_url)
+    game_soup = BeautifulSoup(game_page.text, 'html.parser')
+
+    game_text = game_soup.find("div", class_="icon16_nbrplayer").parent.find_all(text=True)
+
+    number_of_players = game_text[2]
+    average_duration = game_text[4]
+    complexity = game_text[6]
+
+    print(f"{game_href=}")
+    print(f"{number_of_players=}")
+    print(f"{average_duration=}")
+    print(f"{complexity=}")
